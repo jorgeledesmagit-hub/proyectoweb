06711d7284f1f5f2cc4cbedfacd3405bd199bd8a
Jorge <jorge@MacBook-Pro-de-Jorge.local>
2025-07-15 20:10:53 -0300

    Test de sanidad index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
+echo "<h1>¡Hola desde el index.php de Vercel!</h1>";
+die(); // Esto detiene la ejecución del script aquí
+// Resto de tu código PHP
 require_once 'includes/db.php';
 require_once 'includes/carrito.php';
 // Obtener productos destacados (los más recientes)
